8546009cacb32741664cea27ce0d312b34a2e2ae
wangbeiegmail <wangbeie@gmail.com>
2016-01-28 11:26:47 -0800

    project demo
    Here is a MVC structure demo which includes website,server,and database
    connection

---
+++ GISTest/src/edu/uw/wangbei/dao/DBUtils.java
@@ -0,0 +1,16 @@
+package edu.uw.wangbei.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBUtils {
+	public static Connection getConnection() throws Exception {
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/test?user=root&password=root");
+			return conn;
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+}
---
+++ GISTest/src/edu/uw/wangbei/dao/StudentDAO.java
@@ -0,0 +1,37 @@
+package edu.uw.wangbei.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import edu.uw.wangbei.model.Student;
+
+public class StudentDAO {
+	public Student queryStudent(int studentID) throws Exception {
+		
+		Connection conn = DBUtils.getConnection();
+		
+		Statement stmt = null;
+		try {
+			
+			stmt = conn.createStatement();
+			
+			ResultSet rs = stmt.executeQuery("select * from student where studentID = " + studentID);
+			
+			if(rs.next()) {
+				Student student = new Student(rs.getInt("studentID"), rs.getString("studentName"), rs.getInt("age"), rs.getString("Studentcol"));
+				return student;
+			}
+			
+			return null;
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			try {
+				stmt.close();
+				conn.close();
+			} catch(Exception e) {
+			}
+		}
+	}
+}
