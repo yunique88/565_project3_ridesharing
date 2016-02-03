dfd26eaeaed9de39ece9089767557e49e7c155b6
yunique88 <yunique88@gmail.com>
2016-02-03 14:54:09 -0800

    Map inserted

--- GISTest/WebContent/login.js
+++ GISTest/WebContent/login.js
@@ -2,3 +2,12 @@ function login() {
         alert("login clicked, fx run in js");
         return true;
     }
+
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
